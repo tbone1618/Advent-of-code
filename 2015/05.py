2bca07edf2ed0eb332c4c255403bdf810888b57d
tbone1618 <tyhyland@hotmail.com>
2024-12-29 13:19:12 -0700

    day 5 part one complete

--- 2015/05.py
+++ 2015/05.py
@@ -1,11 +1,8 @@
 import re
 # An exercise in regex
 
-file = open("./2015/05.txt")
-data = file.read()
-file.close()
-
-example = "ugknbfddgicrmopn"
+with open('./2015/05.txt') as f:
+    data = [line.rstrip('\n') for line in f]
 
 '''
     1. It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
@@ -13,9 +10,54 @@ example = "ugknbfddgicrmopn"
     3. It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
 '''
 
-# 1: Check string for at least 3 vowels
-# using a set [] searches for any instances of a character in that set
-match = re.findall(r'[aeiou]', example)
-num_found = len(match)
-print(f'vowel matches: {match}')
-print(f'number of vowels: {num_found}')
+def exploration():
+  example = "ugknbfddgicrmopn"
+
+  # 1: Check string for at least 3 vowels
+  # using a set [] searches for any instances of a character in that set
+  match = re.findall(r'[aeiou]', example)
+  num_found = len(match)
+  print('Check 1: looking for 3 or more vowels')
+  print(f'vowel matches: {match}')
+  print(f'number of vowels: {num_found}')
+
+  # 2. Check string for letter twice in a row
+  # (.) captures any character. 
+  # \1 references that character, and 
+  # {1,} checks for 1 or more occurrences of the captured character following that character
+  match = re.search(r'(.)\1{1,}', example)
+  print('Part 2: checking for a double occurrence of a letter')
+  print(match)
+
+  # 3. Check string for instances of substrings
+  naughty_strings = ["ab", "cd", "pq", "xy"]
+  print("Check 3: checking for naughty strings")
+  print(re.findall(r"(?=("+'|'.join(naughty_strings)+r"))", example))
+
+#Putting it all together
+def partOne():
+  total_nice_strings = 0
+  naughty_strings = ["ab", "cd", "pq", "xy"]
+
+  print("Part One: Counting the nice words in Santa's List")
+
+  for word in data:
+    # print()
+    # print("Word to check:")
+    # print(word)
+    match = re.findall(r'[aeiou]', word)
+    num_found = len(match)
+    if num_found > 2:
+      # print("passed first check")
+      match = re.search(r'(.)\1{1,}', word)
+      if match:
+        # print("passed second check")
+        if not any(naughty in word for naughty in naughty_strings):
+          # print("passed third check")
+          total_nice_strings = total_nice_strings + 1
+
+  print(f'Total nice strings in list: {total_nice_strings}')
+  print()
+
+# exploration()
+# partOne()
