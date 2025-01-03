64c6431217829a2d18212b694f13ed99f9b21450
tbone1618 <tyhyland@hotmail.com>
2025-01-02 17:07:20 -0700

    2015 day 6 part 2 complete

--- 2015/06.py
+++ 2015/06.py
@@ -64,7 +64,7 @@ def partOne():
 
   def toggleLights(x1: int, y1: int, x2: int, y2:int, lightArray: list[list[bool]]):
     """
-    will invert the lights in the inputted square
+    will invert the lights in the inputted region
     """
     for i in range (x1, x2+1):
       for j in range (y1, y2+1):
@@ -97,4 +97,91 @@ def partOne():
 
   print(christmasLights.sum())
 
-partOne()
+
+
+
+def partTwo():
+  def parseInput(stringToParse: str) -> list:
+    operation = ""
+    parsedList = []
+
+    split = stringToParse.split()
+    if split[0] == "toggle":
+      operation = "toggle"
+      firstCoordinates = split[1].split(",")
+      secondCoordinates = split[3].split(",")
+    elif split[1] == "on":
+      operation = "on"
+      firstCoordinates = split[2].split(",")
+      secondCoordinates = split[4].split(",")
+    elif split[1] == "off":
+      operation = "off"
+      firstCoordinates = split[2].split(",")
+      secondCoordinates = split[4].split(",")
+    else:
+      print("Invalid string to parse")
+      operation = "invalid"
+
+    parsedList.append(operation)
+    parsedList.extend(firstCoordinates)
+    parsedList.extend(secondCoordinates)
+
+    return(parsedList)
+
+
+  def switchOnLights(x1: int, y1: int, x2: int, y2:int, lightArray: list[list[int]]):
+    """
+    Turns on all lights in the square (x1, y1) through (x2, y2)
+    args: ...
+    """
+
+    for i in range (x1, x2+1):
+      for j in range (y1, y2+1):
+        lightArray[i][j] += 1
+
+  def switchOffLights(x1: int, y1: int, x2: int, y2:int, lightArray: list[list[int]]):
+    """
+    Operates similar to switch on, but will turn lights off instead.
+    """
+    for i in range (x1, x2+1):
+      for j in range (y1, y2+1):
+        if lightArray[i][j] > 0:
+          lightArray[i][j] -= 1
+
+  def toggleLights(x1: int, y1: int, x2: int, y2:int, lightArray: list[list[int]]):
+    """
+    will invert the lights in the inputted region
+    """
+    for i in range (x1, x2+1):
+      for j in range (y1, y2+1):
+        lightArray[i][j] += 2
+
+  # switchOnLights(2, 2, 4, 4, example_lights)
+  # print(example_lights)
+  # switchOffLights(2, 2, 3, 3, example_lights)
+  # print(example_lights)
+  # toggleLights(2, 2, 4, 4, example_lights)
+  # print(example_lights)
+  # print(parseInput(example_toggle))
+
+  christmasLights = np.zeros((1000,1000), dtype=int)
+
+  for line in data:
+    instructions = parseInput(line)
+
+    x1 = int(instructions[1])
+    y1 = int(instructions[2])
+    x2 = int(instructions[3])
+    y2 = int(instructions[4])
+
+    if instructions[0] == "toggle":
+      toggleLights(x1, y1, x2, y2, christmasLights)
+    elif instructions[0] == "on":
+      switchOnLights(x1, y1, x2, y2, christmasLights)
+    elif instructions[0] == "off":
+      switchOffLights(x1, y1, x2, y2, christmasLights)
+
+  print(christmasLights.sum())
+
+# partOne()
+# partTwo()
