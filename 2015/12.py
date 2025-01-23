5c7e943918930e16092d2030eb0d358dbd53053f
tbone1618 <tyhyland@hotmail.com>
2025-01-23 11:58:15 -0700

    2015 day 12 complete

--- 2015/12.py
+++ 2015/12.py
@@ -4,34 +4,22 @@ with open('./2015/12.json', 'r') as file:
     data = json.load(file)
 
 def countNumbers(data):
+
+  if isinstance(data, int):
+     return data
+
+  if isinstance(data, str):
+     return 0 
   
-  sum = 0
-
-  for item in data:
-      # Types to consider:
-      # Dictionary {}
-      # List []
-      # String ""
-      # Int 0
-
-      # Can use the map() function on iterablews
-
-      if type(item) == type({}):
-         # TODO 
-         pass
-      
-      if type(item) == type([]):
-         # TODO
-         pass
-      
-      if type(item) == type(""):
-         return 0
-      
-      if type(item) == type(0):
-         return item
-         
-      pass
-          
-  return sum
+  if isinstance(data, list):
+     return sum(countNumbers(item) for item in data)
+  
+  if isinstance(data, dict):
+     for value in data.values():
+        if value == "red":
+           return 0
+     return sum(countNumbers(value) for value in data.values())
+  
+  return 0
 
 print(countNumbers(data))
